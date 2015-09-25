1f48862848188ff964c65fed39268ee0c9011060
James <jgbentley10@gmail.com>
2015-09-25 15:44:28 +0100

    changed right state

--- feedback/src/rightState.cpp
+++ feedback/src/rightState.cpp
@@ -1,60 +1,90 @@
-//
-//  rightState.cpp
-//  feedback
-//
-//  Created by James Bentley on 9/23/15.
-//
-//
+#include "rightstate.h"
 
-#include "rightState.h"
+using namespace ofxCv;
+using namespace cv;
+
+const float dyingTime = 1;
+
+void Glow::setup(const cv::Rect& track) {
+    color.setHsb(ofRandom(0, 255), 255, 255);
+    left = toOf(track).getCenter().x;
+    deathPoint = toOf(track).getCenter().x;
+    width = 0.0;
+}
+
+void Glow::update(const cv::Rect& track) {
+    left.target(toOf(track).getLeft());
+    width.target(toOf(track).getWidth());
+
+    left.update();
+    width.update();
+}
+
+void Glow::kill() {
+    float curTime = ofGetElapsedTimef();
+    if(startedDying == 0) {
+        startedDying = curTime;
+        deathPoint = left.val + width.val/2;
+    } else if(curTime - startedDying > dyingTime) {
+        dead = true;
+    }
+}
+
+void Glow::draw() {
+    ofPushStyle();
+    ofFill();
+    if(startedDying) {
+        left.target(deathPoint);
+        width.target(0);
+        left.update();
+        width.update();
+    }
+//    float size = 16;
+    ofSetColor(255);
+    ofRect(left.val, 0, width.val, ofGetHeight());
+    ofPopStyle();
+}
 
 void rightState::setup() {
     dimFac = 1.0;
-    percentage = 0;
-    maxArea = 0;
-    percentage.force = 0.005;
-    percentage.target(ofGetWidth());
-}
-
-void rightState::stateEnter() {
+    ofSetVerticalSync(true);
     ofBackground(0);
-    ofSetBackgroundAuto(true);
+    
+    contourFinder.setMinAreaRadius(1);
+    contourFinder.setMaxAreaRadius(100);
+    contourFinder.setThreshold(15);
+    
+    // wait for half a frame before forgetting something
+    tracker.setPersistence(15);
+    // an object can move up to 50 pixels per frame
+    tracker.setMaximumDistance(50);
 }
 
 void rightState::update() {
     getSharedData().updateCamera(dimFac);
-    getSharedData().findCountours();
-    maxArea = 0;
-    for(int i=0; i < getSharedData().finder.size(); i++) {
-        float area = getSharedData().finder.getContourArea(i);
-        maxArea = (area > maxArea) ? area : maxArea;
-        if(maxArea == area) {
-            ofRectangle rect = toOf(getSharedData().finder.getBoundingRect(i));
-            float newPercentage = (rect.x + rect.width) / getSharedData().greyDiff.rows;
-            percentage.target(newPercentage*ofGetWidth());
-        }
+    if(!getSharedData().smallFrame.empty()) {
+        //blur(movie, 10);
+        contourFinder.findContours(getSharedData().smallFrame);
+        tracker.track(contourFinder.getBoundingRects());
     }
-    percentage.update();
 }
 
 void rightState::draw() {
     ofSetColor(255);
-    ofRect(ofGetWidth() - percentage.val, 0, ofGetWidth(), ofGetHeight());
-//    drawMat(getSharedData().frame, 320*1, 0);
-//    drawMat(getSharedData().greyFrame, 320*2, 0);
-//    drawMat(getSharedData().greyBackground, 320*3, 0);
-//    drawMat(getSharedData().greyDiff, 320*4, 0);
- //   ofSetColor(255, 0, 0);
- //   getSharedData().finder.draw();
-}
-
-string rightState::getName() {
-    return "rightState";
+    ofScale(ofGetWidth()/getSharedData().smallFrame.cols, ofGetHeight()/getSharedData().smallFrame.rows);
+//    contourFinder.draw();
+    vector<Glow>& followers = tracker.getFollowers();
+    for(int i = 0; i < followers.size(); i++) {
+        followers[i].draw();
+    }
 }
 
 void rightState::keyPressed(int key) {
     getSharedData().changeInitialState(key);
     getSharedData().changeThreshold(key);
     changeState(getSharedData().initialState);
-    if(key == ' ') getSharedData().captureBackground();
+}
+
+string rightState::getName() {
+    return "rightState";
 }
--- feedback/src/rightState.h
+++ feedback/src/rightState.h
@@ -1,26 +1,35 @@
-//
-//  rightState.h
-//  feedback
-//
-//  Created by James Bentley on 9/23/15.
-//
-//
-
 #pragma once
 
 #include "ofxIntegrator.h"
 #include "ofxState.h"
 #include "sharedData.h"
 
+class Glow : public ofxCv::RectFollower {
+protected:
+    ofColor color;
+    Integrator <float> left, width;
+    float startedDying;
+    float deathPoint;
+public:
+    Glow()
+    :startedDying(0) {
+    }
+    void setup(const cv::Rect& track);
+    void update(const cv::Rect& track);
+    void kill();
+    void draw();
+};
+
 class rightState : public itg::ofxState<sharedData> {
 public:
     void setup();
     void update();
     void draw();
-    void keyPressed(int key);
-    void stateEnter();
     string getName();
+    void keyPressed(int key);
     
-    float dimFac, maxArea;
-    Integrator<float> percentage;
+    float dimFac;
+    
+    ofxCv::ContourFinder contourFinder;
+    ofxCv::RectTrackerFollower<Glow> tracker;
 };
